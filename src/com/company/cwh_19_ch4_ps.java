68c8b1eeaa2e66a833a08ed84337b75ee701af75
Suraj Rathod <87470519+surajvrathod@users.noreply.github.com>
2021-09-28 15:23:43 +0530

    Cwh_19_ch4_ps.java

--- src/com/company/cwh_19_ch4_ps.java
+++ src/com/company/cwh_19_ch4_ps.java
@@ -28,5 +28,97 @@ public class cwh_19_ch4_ps {
                 System.out.println("Sorry,You are failed");
         else
             System.out.println("Sorry,You are failed");
+
+        /* Question 3 :Calculate income tax paid by an employee to the government as per the slabs mentioned below:
+
+        Income Slab	Tax
+        2.5L – 5.0L  	5%
+        5.0L – 10.0L 	20%
+        Above 10.0L	30%
+        Note that there is not tax below 2.5L. Take the input amount as input from the user. */
+        System.out.println("Enter your in lakhs");
+        float salary =sc.nextFloat();
+        float tax=0.0f;
+        if(salary<2.5)
+            System.out.println("You are exempted for tax");
+        else if(salary>=2.5 && salary<=5)
+        {
+            tax=salary*5000f;
+            System.out.println("Tax to be paid is "+tax+" Rupees");
+        }
+        else if(salary>=5 && salary<=10)
+        {
+            tax=salary*20000f;
+            System.out.println("Tax to be paid is "+tax+" Rupees");
+        } else if(salary>=10 )
+        {
+            tax=salary*30000f;
+            System.out.println("Tax to be paid is "+tax+" Rupees");
+        }
+
+        //Question 4: Write a Java program to find out the day of the week given the number [1 for Monday, 2 for Tuesday … and so on!]
+
+
+//        System.out.println("Please Enter 1 to 7");
+//        int day= sc.nextInt();
+//        switch (day){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//                case 3:
+//                System.out.println("Wednesday");
+//                break;
+//                case 4:
+//                System.out.println("Thursday");
+//                break;
+//                case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Please Enter valid input");
+//                break;
+//              }
+
+                //Question 5:Write a Java program to find whether a year entered by the user is a leap year or not.
+            System.out.println("Please Enter the year");
+            int year=sc.nextInt();
+            if(year%100==0 && year%400==0 || year%100!=0 &&year % 4 == 0){
+                System.out.println("Leap Year");
+            }
+            else
+            {
+                System.out.println("Not a Leap year");
+            }
+
+            //Question 6:Write a program to find out the type of website from the URL:
+        //
+        //.com – commercial website
+        //.org – organization website
+        //.in – Indian website
+        System.out.println("Please enter your website");
+            String website= sc.next();
+
+            if (website.endsWith(".com")){
+                System.out.println("Entered website is a Commercial Website");
+
+        }
+        else if (website.endsWith(".org")){
+            System.out.println("Entered website is a orgnizational Website");
+
+        }
+        if (website.endsWith(".in")){
+            System.out.println("Entered website is a indian Website");
+        }
+
+        }
+
     }
-}
